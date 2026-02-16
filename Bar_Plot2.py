f50a99b73eecb8c6bae69a9ae07aea1cc92f51df
Rishikesh7713 <rishikeshpramoth@gmail.com>
2026-02-16 18:26:34 +0530

    Add files via upload

--- Bar_Plot2.py
+++ Bar_Plot2.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 day=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 nb1=[12, 15, 11, 9, 1, 9, 21]
-nb2=[17, 5, 2, 11, 1, 8, 29]
 plt.figure(figsize=(15, 9))
 plt.bar(day, nb1, width=0.6)
 plt.xlabel("Cfsfe")
 plt.ylabel("Population Growth (rhrhthMillions)")
 plt.title("Top 10 countries with the Bigggfhjyxjrgggggggest Population between 1952 to 2007")
 plt.xticks(rotation=45)
+plt.show()
